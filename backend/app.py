b092cb77984748b350c18c1e0ffa483052bd3cce
Saiharini <saiharinipoosala1415@gmail.com>
2026-06-08 15:44:27 +0530

    Initial RAG pipeline: PDF to FAISS to Gemini working

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,71 @@
-from google import genai
 from dotenv import load_dotenv
+from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
 import os
 
-# Load .env file
 load_dotenv()
 
-# Create Gemini client
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
+# Read PDF
+reader = PdfReader("../docs/UI_UX_Resume.pdf")
+
+text = ""
+for page in reader.pages:
+    page_text = page.extract_text()
+    if page_text:
+        text += page_text
+
+# Chunking
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 200,
+    chunk_overlap = 50
 )
 
-try:
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Say Hello"
-    )
+chunks = splitter.split_text(text)
 
-    print("\nGemini Response:\n")
-    print(response.text)
+print("Total Chunks:", len(chunks))
 
-except Exception as e:
-    print("\nError:")
-    print(e)
+# Embeddings
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001",
+    google_api_key=os.getenv("GEMINI_API_KEY")
+)
+
+# Store in FAISS
+vectorstore = FAISS.from_texts(
+    chunks,
+    embedding=embeddings
+)
+
+query = "What skills does this person have?"
+
+docs = vectorstore.similarity_search(query, k=2)
+
+print("\nTOP RETRIEVED CHUNKS:\n")
+for i, doc in enumerate(docs):
+    print(f"Chunk {i+1}:\n{doc.page_content}\n")
+
+# Build context properly
+context = "\n".join([doc.page_content for doc in docs])
+
+# LLM
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY")
+)
+
+prompt = f"""
+Answer the question based only on the context below.
+
+Context:
+{context}
+
+Question:
+{query}
+"""
+
+response = llm.invoke(prompt)
+
+print("\nAI ANSWER:\n")
+print(response.content)
