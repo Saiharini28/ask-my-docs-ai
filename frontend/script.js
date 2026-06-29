8117031c5e9ab31746f0070e0b8acf57a7a4660a
Saiharini <saiharinipoosala1415@gmail.com>
2026-06-29 11:58:21 +0530

    Added chat history feature

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,11 @@
+
+let chats =
+    JSON.parse(
+        localStorage.getItem("chats")
+    ) || [];
+
+let currentChat = null;
+
 const API_URL = "http://127.0.0.1:5000";
 
 async function uploadPDF() {
@@ -56,6 +64,20 @@ async function sendMessage() {
             ${question}
         </div>
     `;
+    if (currentChat) {
+        const chat =
+            chats.find(
+                c => c.id === currentChat
+            );
+
+        if (chat) {
+
+            chat.messages =
+                chatBox.innerHTML;
+
+            saveChats();
+        }
+    }
 
     questionInput.value = "";
 
@@ -83,6 +105,24 @@ async function sendMessage() {
                 ${data.answer}
             </div>
         `;
+        const chat =
+        chats.find(
+            c => c.id === currentChat
+        );
+        
+        if(chat){
+            chat.messages =
+            chatBox.innerHTML;
+            if(chat.title === "New Chat"){
+                chat.title =
+                question.substring(
+                    0,
+                    20
+                );
+            }
+            saveChats();
+            renderChats();
+        }
 
         chatBox.scrollTop =
             chatBox.scrollHeight;
@@ -112,7 +152,21 @@ document
     }
 );
 
-function newChat(){
+function newChat() {
+
+    const id = Date.now();
+
+    currentChat = id;
+
+    chats.unshift({
+        id: id,
+        title: "New Chat",
+        messages: ""
+    });
+
+    saveChats();
+
+    renderChats();
 
     document.getElementById(
         "chatBox"
@@ -121,7 +175,11 @@ function newChat(){
             New Chat Started
         </div>
     `;
-
+    chats[0].messages =
+        document.getElementById(
+            "chatBox"
+        ).innerHTML;
+    saveChats();
 }
 
 async function loadDocuments() {
@@ -147,3 +205,54 @@ async function loadDocuments() {
 }
 
 loadDocuments();
+
+function saveChats() {
+
+    localStorage.setItem(
+        "chats",
+        JSON.stringify(chats)
+    );
+}
+
+function renderChats() {
+
+    const history =
+        document.getElementById(
+            "chatHistory"
+        );
+
+    history.innerHTML = "";
+
+    chats.forEach(chat => {
+
+        const div =
+            document.createElement("div");
+
+        div.className = "doc-item";
+
+        div.innerText = chat.title;
+
+        div.onclick = () => {
+
+            currentChat = chat.id;
+
+            document.getElementById(
+                "chatBox"
+            ).innerHTML = chat.messages;
+        };
+
+        history.appendChild(div);
+    });
+}
+
+renderChats();
+
+if (chats.length > 0) {
+
+    currentChat = chats[0].id;
+
+    document.getElementById(
+        "chatBox"
+    ).innerHTML =
+        chats[0].messages;
+}
